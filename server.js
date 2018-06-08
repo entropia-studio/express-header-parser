746d3668de7ae336a84a7d3c4cd93eaafa4cbf1a
entropia.studio <info@entropia.studio>
2018-06-08 11:02:16 +0000

    Add cors for remote testing. Add response to json object

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express")
 
 var app = express()
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
 app.use(express.static('public'))
 
 app.get("/", function (request, response) {
@@ -18,7 +23,7 @@ app.get("/api/whoami",(request,response) => {
         "language" : request.headers["accept-language"],
         "software" : request.headers["user-agent"]
     })
-    response.status(200).send(jsonResponse)
+    response.status(200).json(jsonResponse)
 })
 
 app.listen(8080,'localhost')
